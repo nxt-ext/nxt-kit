d7fd7bed22eb28288067af6236439a9dd625699f
nxt-ext <nxtcoin@yandex.com>
2014-01-02 11:13:07 -0500

    Merged scripts. Cache is cleared after restoring

--- scripts-src/check.sh
+++ scripts-src/check.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-if [ -z "$(pgrep java)" ] || [ -z "$(wget -qO- http://localhost:7874/nxt?requestType=getState)" ]
-  then
-     # nxt is not running
-    echo 'ERROR: nxt is NOT running correctly'
-    ~/nxt-kit/sbin/restart.nxt.sh
-    echo 'RESTARTED: nxt'
-  else
-     # nxt is running
-    echo "OK: nxt is running"
+if [[ "`pidof -x $(basename $0) -o %PPID`" ]]; then exit 0; fi
+cd ~/nxt-kit/nxt
+pid=$(pgrep -f 'java -jar start.jar')
+state=$(wget -qO- http://localhost:7874/nxt?requestType=getState)
+if [[ $state ]] && [[ $pid ]]; then
+  echo 'OK: caching chain'
+  tar -czvf ../distrib/chain.tar.gz blockchain.nrs blocks.nxt transactions.nxt
+else
+  echo 'ERROR: nxt is NOT running correctly'
+  pkill -f 'java -jar start.jar'
+  rm -f blockchain.nrs blocks.nxt transactions.nxt
+  if [ -f ../distrib/chain.tar.gz ]; then
+    echo 'Restoring cached chain'
+    tar -xzvf ../distrib/chain.tar.gz
+    rm -f ../distrib/chain.tar.gz
+  elif [ -f ../distrib/chain-original.tar.gz ]; then
+    echo 'Restoring original chain'
+    tar -xzvf ../distrib/chain-original.tar.gz
+  fi
+  nohup java -jar start.jar &
 fi
