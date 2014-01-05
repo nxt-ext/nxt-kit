ee4e7616852fb140462e406dbd8b88472a07d8ca
nxt-ext <nxtcoin@yandex.com>
2014-01-05 04:54:55 -0500

    0.5.0 java.lang.NullPointerException workaround

--- scripts-src/check.sh
+++ scripts-src/check.sh
@@ -3,7 +3,7 @@ if [[ "`pidof -x $(basename $0) -o %PPID`" ]]; then exit 0; fi
 cd ~/nxt-kit/nxt
 pid=$(pgrep -f 'java -jar start.jar')
 state=$(wget -qO- http://localhost:7874/nxt?requestType=getState)
-if [[ $state ]] && [[ $pid ]]; then
+if [[ $state ]] && [[ $state != *\"numberOfBlocks\":0* ]] && [[ $pid ]]; then
   echo 'OK: caching chain'
   tar -czvf ../distrib/chain.tar.gz blockchain.nrs blocks.nxt transactions.nxt
 else
