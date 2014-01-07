1ecd9d77b7ca84093f8444e771cd39fa84fcc9e7
nxt-ext <nxtcoin@yandex.com>
2014-01-07 07:25:32 -0500

    Restart after three hours without new block

--- scripts-src/check.sh
+++ scripts-src/check.sh
@@ -2,10 +2,32 @@
 if [[ "`pidof -x $(basename $0) -o %PPID`" ]]; then exit 0; fi
 cd ~/nxt-kit/nxt
 pid=$(pgrep -f 'java -jar start.jar')
-state=$(wget -qO- http://localhost:7874/nxt?requestType=getState)
-if [[ $state ]] && [[ $state != *\"numberOfBlocks\":0* ]] && [[ $pid ]]; then
-  echo 'OK: caching chain'
-  tar -czvf ../distrib/chain.tar.gz blockchain.nrs blocks.nxt transactions.nxt
+typeset -i curr_block_id=$(wget -qO- http://localhost:7874/nxt?requestType=getState | grep -oP '"numberOfBlocks":\d+' | awk -F ":" '{print $2}')
+if [[ $curr_block_id != 0 ]] && [[ $pid ]]; then
+  prev_block_id=0
+  if [ -f ../distrib/blockID ]; then
+      typeset -i prev_block_id=$(cat ../distrib/blockID)
+  fi
+  if [[ $curr_block_id != $prev_block_id ]]; then
+    echo 'OK: caching chain'
+    echo $curr_block_id > ../distrib/blockID
+    echo 1 > ../distrib/blockCnt
+    tar -czvf ../distrib/chain.tar.gz blockchain.nrs blocks.nxt transactions.nxt
+  else
+    prev_block_cnt=0
+    if [ -f ../distrib/blockCnt ]; then
+        typeset -i prev_block_cnt=$(cat ../distrib/blockCnt)
+    fi
+    if [[ $prev_block_cnt < 180 ]]; then
+      ((prev_block_cnt++))
+      echo $prev_block_cnt > ../distrib/blockCnt
+      echo "OK: block repeated $prev_block_cnt times"
+    else
+       echo "ERROR: I can't stand this block anymore"
+       pkill -f 'java -jar start.jar'
+       rm -f ../distrib/chain.tar.gz
+    fi
+  fi
 else
   echo 'ERROR: nxt is NOT running correctly'
   pkill -f 'java -jar start.jar'
