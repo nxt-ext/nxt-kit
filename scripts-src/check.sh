2f8ab6bc8c13a2b2f909a4bfbaa227bf9fd2a334
nxt-ext <nxtcoin@yandex.com>
2014-02-26 02:41:51 -0500

    Upd: got rid of openshift.check.sh

--- scripts-src/check.sh
+++ scripts-src/check.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [[ "`pidof -x $(basename $0) -o %PPID`" ]]; then exit 0; fi
 cd {{ nxt_remote_folder }}/nxt
-typeset -i curr_block_id=$(wget -qO- http://localhost:7876/nxt?requestType=getState | grep -oP '"numberOfBlocks":\d+' | awk -F ":" '{print $2}')
+typeset -i curr_block_id=$(wget -qO- http://{{ (kit_ServerHost.stdout|default(kit_ServerHost)) if kit_ServerHost is defined else "localhost" }}:{{ kit_apiServerPort if kit_apiServerPort is defined else 7876 }}/nxt?requestType=getState | grep -oP '"numberOfBlocks":\d+' | awk -F ":" '{print $2}')
 if (( $curr_block_id != 0 )); then
   prev_block_id=0
   if [ -f ../distrib/blockID ]; then
       typeset -i prev_block_id=$(cat ../distrib/blockID)
   fi
   if (( $curr_block_id != $prev_block_id )); then
-    echo "$(date) OK: caching chain" >> ../distrib/cron.log
+    echo "$(date) OK: caching chain with block $curr_block_id" >> ../distrib/cron.log
     echo $curr_block_id > ../distrib/blockID
     echo 1 > ../distrib/blockCnt
     tar -czvf ../distrib/chain.tar.gz nxt_db/
@@ -20,9 +20,9 @@ if (( $curr_block_id != 0 )); then
     if (( $prev_block_cnt < 60 )); then
       ((prev_block_cnt++))
       echo $prev_block_cnt > ../distrib/blockCnt
-      echo "$(date) OK: block repeated $prev_block_cnt times" >> ../distrib/cron.log
+      echo "$(date) OK: block $curr_block_id repeated $prev_block_cnt times" >> ../distrib/cron.log
     else
-       echo "$(date) ERROR: I can't stand this block anymore" >> ../distrib/cron.log
+       echo "$(date) ERROR: I can't stand block $curr_block_id anymore" >> ../distrib/cron.log
        # Obsolete and will be removed
        pkill -f 'java -jar start.jar'
        pkill -f 'nxt.jar'
