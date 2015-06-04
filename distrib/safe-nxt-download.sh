3e9ccec98919906b56c01481f3fd3962a8fc7918
EmoneyRu <emonru@ya.ru>
2015-06-04 00:40:28 -0400

    Fix: signature check since 1.5.10

--- distrib/safe-nxt-download.sh
+++ distrib/safe-nxt-download.sh
@@ -6,7 +6,7 @@ fi
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
 gpg --recv-keys 0xFF2A19FA > /dev/null 2>&1 || { echo "Could not recieve public key for file verification. Exiting."; exit 1; }
 rm -f nxt-client-$1.* nxt.zip
-wget -nv https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-$1.{zip,zip.asc,changelog.txt.asc} > /dev/null 2>&1
+wget -nv https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-$1.{zip,jar,zip.asc,changelog.txt.asc} > /dev/null 2>&1
 zip_sig=$(gpg --verify nxt-client-$1.zip.asc > /dev/null 2>&1; echo $?)
 chg_sig=$(gpg --verify nxt-client-$1.changelog.txt.asc > /dev/null 2>&1; echo $?)
 sha_sum=$(sha256sum -c nxt-client-$1.changelog.txt.asc > /dev/null 2>&1; echo $?)
