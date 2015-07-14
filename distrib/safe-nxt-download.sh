80333d2b0947464d108acd29c8b9cc96e357430c
EmoneyRu <emonru@ya.ru>
2015-07-14 07:07:16 +0300

    Upd: check nxt-client-*.jar signature

--- distrib/safe-nxt-download.sh
+++ distrib/safe-nxt-download.sh
@@ -6,11 +6,12 @@ fi
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
 gpg --recv-keys 0xFF2A19FA > /dev/null 2>&1 || { echo "Could not recieve public key for file verification. Exiting."; exit 1; }
 rm -f nxt-client-$1.* nxt.zip
-wget -nv https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-$1.{zip,jar,zip.asc,changelog.txt.asc} > /dev/null 2>&1
+wget -nv https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-$1.{zip,jar,zip.asc,jar.asc,changelog.txt.asc} > /dev/null 2>&1
 zip_sig=$(gpg --verify nxt-client-$1.zip.asc > /dev/null 2>&1; echo $?)
+jar_sig=$(gpg --verify nxt-client-$1.jar.asc > /dev/null 2>&1; echo $?)
 chg_sig=$(gpg --verify nxt-client-$1.changelog.txt.asc > /dev/null 2>&1; echo $?)
 sha_sum=$(sha256sum -c nxt-client-$1.changelog.txt.asc > /dev/null 2>&1; echo $?)
-if [[ sha_sum -ne 0 || zip_sig -ne 0 || chg_sig -ne 0 ]]; then
+if [[ sha_sum -ne 0 || jar_sig -ne 0 || zip_sig -ne 0 || chg_sig -ne 0 ]]; then
 	rm -f nxt-client-$1.*
 	echo "ERROR: Signature mismatch."
 	exit 1
