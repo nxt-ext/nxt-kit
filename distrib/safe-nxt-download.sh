254f85a5e598864e585c56d886c31a786a53d35c
EmoneyRu <emonru@ya.ru>
2016-04-08 19:08:30 +0300

    Upd: replace `jar` installer with `sh` in download script

--- distrib/safe-nxt-download.sh
+++ distrib/safe-nxt-download.sh
@@ -6,9 +6,9 @@ fi
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
 gpg --recv-keys 0xB88DC0A62ECDDDD1643A28FDCEF1F4A9FF2A19FA 0x263A9EB029CFC77A3D06FD13811D6940E1E4240C > /dev/null 2>&1 || { echo "Could not recieve public key for file verification. Exiting."; exit 1; }
 rm -f nxt-client-$1.* nxt.zip
-wget -nv https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-$1.{zip,jar,zip.asc,jar.asc,changelog.txt.asc} > /dev/null 2>&1
+wget -nv https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-$1.{zip,sh,zip.asc,sh.asc,changelog.txt.asc} > /dev/null 2>&1
 zip_sig=$(gpg --verify nxt-client-$1.zip.asc > /dev/null 2>&1; echo $?)
-jar_sig=$(gpg --verify nxt-client-$1.jar.asc > /dev/null 2>&1; echo $?)
+jar_sig=$(gpg --verify nxt-client-$1.sh.asc > /dev/null 2>&1; echo $?)
 chg_sig=$(gpg --verify nxt-client-$1.changelog.txt.asc > /dev/null 2>&1; echo $?)
 sha_sum=$(sha256sum -c nxt-client-$1.changelog.txt.asc > /dev/null 2>&1; echo $?)
 if [[ sha_sum -ne 0 || jar_sig -ne 0 || zip_sig -ne 0 || chg_sig -ne 0 ]]; then
