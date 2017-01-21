cd1f2503a574988e03dee8d6efb0472b5d7b2e74
EmoneyRu <emonru@ya.ru>
2017-01-21 08:35:32 +0300

    Fix: typo in 'safe-nxt-download.sh'

--- distrib/safe-nxt-download.sh
+++ distrib/safe-nxt-download.sh
@@ -8,10 +8,10 @@ gpg --recv-keys 0xB88DC0A62ECDDDD1643A28FDCEF1F4A9FF2A19FA 0x263A9EB029CFC77A3D0
 rm -f nxt-client-$1.* nxt.zip
 wget -nv https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-$1.{zip,sh,zip.asc,sh.asc,changelog.txt.asc} > /dev/null 2>&1
 zip_sig=$(gpg --verify nxt-client-$1.zip.asc > /dev/null 2>&1; echo $?)
-jar_sig=$(gpg --verify nxt-client-$1.sh.asc > /dev/null 2>&1; echo $?)
+sh_sig=$(gpg --verify nxt-client-$1.sh.asc > /dev/null 2>&1; echo $?)
 chg_sig=$(gpg --verify nxt-client-$1.changelog.txt.asc > /dev/null 2>&1; echo $?)
 sha_sum=$(sha256sum -c nxt-client-$1.changelog.txt.asc > /dev/null 2>&1; echo $?)
-if [[ sha_sum -ne 0 || jar_sig -ne 0 || zip_sig -ne 0 || chg_sig -ne 0 ]]; then
+if [[ sha_sum -ne 0 || sh_sig -ne 0 || zip_sig -ne 0 || chg_sig -ne 0 ]]; then
 	rm -f nxt-client-$1.*
 	echo "ERROR: Signature mismatch."
 	exit 1
