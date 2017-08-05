9698bc77f6015c7d91f4345fff0a769303654077
EmoneyRu <emonru@ya.ru>
2017-08-05 08:07:25 +0300

    Upd: migrate to Jelurida Release Signing GPG Key

--- distrib/safe-nxt-download.sh
+++ distrib/safe-nxt-download.sh
@@ -4,7 +4,7 @@ if [[ -z "$1" ]]; then
   exit 1
 fi
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-gpg --recv-keys 0xB88DC0A62ECDDDD1643A28FDCEF1F4A9FF2A19FA 0x263A9EB029CFC77A3D06FD13811D6940E1E4240C > /dev/null 2>&1 || { echo "Could not recieve public key for file verification. Exiting."; exit 1; }
+{ wget -qO - https://www.jelurida.com/sites/default/files/jelurida.gpg | gpg --import - > /dev/null 2>&1; } || { echo "Could not recieve public key for file verification. Exiting."; exit 1; }
 rm -f nxt-client-$1.* nxt.zip
 wget -nv https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-$1.{zip,sh,zip.asc,sh.asc,changelog.txt.asc} > /dev/null 2>&1
 zip_sig=$(gpg --verify nxt-client-$1.zip.asc > /dev/null 2>&1; echo $?)
